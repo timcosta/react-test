aefdfc8385b1b65a0739caf6f18656da5f6426c7
Tim Costa <tcosta@2u.com>
2016-07-22 15:51:36 -0400

    prod size optimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,48 @@
+const webpack = require('webpack');
 var config = {
-   entry: './src/entry.js',
+    devtool: 'cheap-module-source-map',
+    entry: './src/entry.js',
 
-   output: {
-      path:'./dist',
-      filename: 'index.js',
-   },
+    output: {
+        path:'./dist',
+        filename: 'bundle.js',
+    },
 
-   devServer: {
-      inline: true,
-      port: 7000,
-      historyApiFallback: true,
-      proxy: {
-          '/api*': {
-              target: 'http://localhost:8000'
-          }
-      }
-   },
-
-   module: {
-      loaders: [
-         {
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loader: 'babel',
-
-            query: {
-               presets: ['es2015', 'react']
+    devServer: {
+        inline: true,
+        port: 7000,
+        historyApiFallback: true,
+        proxy: {
+            '/api*': {
+                target: 'http://localhost:8000'
             }
-         }
-      ],
-      noParse: [
-          /node_modules[\/]video\.js[\/]/
-      ]
-   }
+        }
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+
+                query: {
+                presets: ['es2015', 'react']
+                }
+            }
+        ],
+        noParse: [
+            /node_modules[\/]video\.js[\/]/
+        ]
+    },
+
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        })
+    ]
 }
 
 module.exports = config;
